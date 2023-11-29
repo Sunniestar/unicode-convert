14a30b7c4096e590834fdaf1d236c15fd77d1a39
Sunniestar <143650691+Sunniestar@users.noreply.github.com>
2023-11-29 10:28:16 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const uni=function uni(codePt) {
+    if (codePt > 0xFFFF) {
+        codePt -= 0x10000;
+        return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF));
+    }
+    else {
+        return String.fromCharCode(codePt);
+    }
+}
+const input0=document.getElementById('input0')
+const input0value=function value(){
+  return document.getElementById('input0').value
+}
